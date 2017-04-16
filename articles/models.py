d0ce39748f4c310833da029d23555b66a4d73190
ygreif <ygreif@gmail.com>
2017-04-16 11:27:15 -0700

    Checkpoint

--- articles/models.py
+++ articles/models.py
@@ -2,4 +2,28 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    title = models.CharField(max_length=200)
+    parent = models.ForeignKey('self', blank=True, null=True)
+    children = models.ManyToManyField('self', blank=True, null=True)
+    articles = models.ManyToManyField('Article', blank=True, null=True)
+    depth = False
+
+    def rows(self):
+        articles = self.articles.all()
+        return [articles[i:i + 3] for i in range(0, len(articles), 3)]
+
+    def __str__(self):
+        return self.title
+
+
+class Article(models.Model):
+    parent_topic = models.ForeignKey(Topic)
+    title = models.TextField(max_length=200)
+    summary = models.TextField()
+    link = models.URLField()
+    img = models.URLField()
+
+    def __str__(self):
+        return self.title
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from .models import Article, Topic
+from .forms import ArticleForm, TopicForm
 # Create your views here.
+
+
+def index(request):
+    topics = Topic.objects.all()
+    articles = Article.objects.all()
+    rows = [articles[i:i + 3] for i in range(0, len(articles), 3)]
+    context = {'topics': topics, 'root': topics[0]}
+    return render(request, 'articles/index.html', context)
+
+
+def new_article(request, topic):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            topic = Topic.objects.get(title=topic)
+            article = form.save(commit=False)
+            article.parent_topic = topic
+            article.save()
+            topic.articles.add(article)
+            topic.save()
+            return redirect('/')
+    else:
+        form = ArticleForm()
+    context = {'form': ArticleForm()}
+    return render(request, 'articles/new_article.html', context)
+
+
+def new_topic(request, topic):
+    if request.method == 'POST':
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            parent_topic = Topic.objects.get(title=topic)
+            child_topic = form.save(commit=False)
+            child_topic.parent = parent_topic
+            child_topic.save()
+            parent_topic.children.add(child_topic)
+            parent_topic.save()
+            return redirect('/')
+    else:
+        form = ArticleForm()
+    context = {'form': TopicForm()}
+    return render(request, 'topics/new_topic.html', context)
