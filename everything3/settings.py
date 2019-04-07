9722895cc1fcb0f32a67510ed811111ff8b76da7
Yaron <ygreif@gmail.com>
2019-04-06 20:57:15 -0700

    Comment out postgres

--- everything3/settings.py
+++ everything3/settings.py
@@ -82,14 +82,14 @@ WSGI_APPLICATION = 'everything3.wsgi.application'
 
 DATABASES = {
     'default': {
-        #        'ENGINE': 'django.db.backends.sqlite3',
-        #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myproject',
-        'USER': 'ygreif',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #        'NAME': 'myproject',
+        #        'USER': 'ygreif',
+        #        'PASSWORD': '123',
+        #        'HOST': 'localhost',
+        #        'PORT': '',
     }
 }
 
