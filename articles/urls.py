9be4cfda5b8cf3214f6d680c480fbd745e9bcc97
Yaron <ygreif@gmail.com>
2019-05-05 20:58:41 -0700

    New something

--- articles/urls.py
+++ articles/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^edit/stub/(.*)/$', views.complete_stub),
-    url(r'^new/something/(.*)/$', views.new_something),
+    url(r'^new/something/$', views.new_something),
     url(r'^new/article/(.*)/$', views.new_article),
     url(r'^new/topic/(.*)/$', views.new_topic),
     url(r'^search/$', views.search),
